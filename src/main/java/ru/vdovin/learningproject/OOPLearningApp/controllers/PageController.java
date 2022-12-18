3b58ad58b6f68c08ec9ae2da31f1ff656e4e9173
Max Vdovin <maxpostupaet@gmail.com>
2022-12-18 13:28:45 +0300

    Fix rendering errors.

--- src/main/java/ru/vdovin/learningproject/OOPLearningApp/controllers/PageController.java
+++ src/main/java/ru/vdovin/learningproject/OOPLearningApp/controllers/PageController.java
@@ -1,13 +1,11 @@
 package ru.vdovin.learningproject.OOPLearningApp.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.vdovin.learningproject.OOPLearningApp.services.AdminService;
 
 @Controller
 public class PageController {
+
     @GetMapping("/logo")
     public String helloPage1() {
         return "css/page1";
